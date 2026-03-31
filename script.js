90e06dbaed94097262b93f9e11c963bac8a23203
Ange <angeluxdk@gmail.com>
2026-03-31 11:46:13 +0000

    Implement automatic and manual slide display
    Add carousel functionality to display slides automatically and manually.

--- script.js
+++ script.js
@@ -69,6 +69,33 @@ function sendWhatsApp() {
     const url = `https://wa.me/22870009306?text=${encodeURIComponent(message)}`;
     window.open(url, '_blank');
 }
+let slideIndex = 0;
+showSlides();
+
+function plusSlides(n) {
+    showSlidesManual(slideIndex += n);
+}
+
+function showSlides() {
+    let slides = document.getElementsByClassName("carousel-slide");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex-1].style.display = "block";  
+    setTimeout(showSlides, 5000); // Change d'image toutes les 5 secondes
+}
+
+function showSlidesManual(n) {
+    let slides = document.getElementsByClassName("carousel-slide");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+}
 
 // Initialisation
 displayProducts();
