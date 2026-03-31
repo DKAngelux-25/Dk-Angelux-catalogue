9fc39d724b75f66ded9d1a86cd6f157bfc3340af
Ange <angeluxdk@gmail.com>
2026-03-31 11:39:05 +0000

    Add products data to products.js

---
+++ products.js
@@ -0,0 +1,24 @@
+const products = [
+    {
+        id: 1,
+        name: "Huile de Tournesol",
+        category: "alimentaire",
+        image: "huile.jpg",
+        description: "Huile pure pression à froid",
+        options: [
+            { type: "Litre", price: 1500, unit: "1L" },
+            { type: "Carton", price: 17000, unit: "12L" }
+        ],
+        isMarketplace: false
+    },
+    {
+        id: 2,
+        name: "Perceuse Bosch Professional",
+        category: "bricolage",
+        image: "perceuse.jpg",
+        options: [
+            { type: "Unité", price: 45000, unit: "1 pièce" }
+        ],
+        isMarketplace: true // Badge spécial partenaire
+    }
+];
