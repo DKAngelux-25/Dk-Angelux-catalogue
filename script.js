2f2fd84b0504ecf654b39a00553986b3f6229d80
Ange <angeluxdk@gmail.com>
2026-03-31 12:33:22 +0000

    Update script.js

--- script.js
+++ script.js
@@ -96,6 +96,33 @@ function showSlidesManual(n) {
     }
     slides[slideIndex-1].style.display = "block";
 }
+let slideIndex = 0;
+showSlides();
+
+function plusSlides(n) {
+    showSlidesManual(slideIndex += n);
+}
+
+function showSlides() {
+    let slides = document.getElementsByClassName("carousel-slide");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex-1].style.display = "block";  
+    setTimeout(showSlides, 5000); // Change d'image toutes les 5 secondes
+}
+
+function showSlidesManual(n) {
+    let slides = document.getElementsByClassName("carousel-slide");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+}
 
 // Initialisation
 displayProducts();
