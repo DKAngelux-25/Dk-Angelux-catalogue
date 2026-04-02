c6642cf62e8fd2f58546da9d6f6edbf713be320b
Ange <angeluxdk@gmail.com>
2026-04-02 17:25:41 +0000

    Update script.js

--- script.js
+++ script.js
@@ -77,7 +77,7 @@ function filterCategory(cat) {
     const header = document.getElementById('category-header');
     header.innerHTML = `
         <div class="category-banner-wrapper">
-            <img src="images/banner-${cat}.jpg" alt="Rayon ${cat}">
+            <img src="images/banner-${cat}.png" alt="Rayon ${cat}">
             <button onclick="resetHome()" class="btn-back">← Retour à l'accueil</button>
         </div>
     `;
